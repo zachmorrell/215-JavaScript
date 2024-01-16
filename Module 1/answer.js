72d2a6fe98b3a839e70cadd726bdd52ff2b691e9
Zachary Morrell <zauruke@gmail.com>
2024-01-16 15:22:36 -0500

    Complete Assignment

---
+++ Module 1/answer.js
@@ -0,0 +1,6 @@
+function answer2() {
+    document.getElementById("answer2").innerHTML = "JavaScript properties that allowed it to surpass other languages:<ul><li>Easy to learn</li><li>Free and an open community</li><li>Allows developers to quickly create scalable apps</li></ul>";
+}
+function answer3() {
+    document.getElementById("answer3").innerHTML = "Popular project created using some form of JavaScript:<ul><li>Facebook</li><li>YouTube</li><li>Google Maps</li><li>eBay</li></ul>Popular Frameworks or libraries supported by JavaScript<ul><li>Ember</li><li>Angular</li><li>React</li><li>Vue</li><li>Node.js</li></ul>";
+}
