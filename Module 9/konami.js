a8bf2b26e432b540d7387f2deee74c3367bb5949
Zachary Morrell <zauruke@gmail.com>
2024-03-15 11:59:42 -0400

    Complete assignment

--- Module 9/konami.js
+++ Module 9/konami.js
@@ -2,7 +2,7 @@
 var konamiCode = [
 	"ArrowUp","ArrowUp","ArrowDown","ArrowDown",
 	"ArrowLeft","ArrowRight","ArrowLeft","ArrowRight",
-	"b","a"
+	"KeyB","KeyA"
 ];
 console.log("Successfully imported.");
 document.getElementById("dom_events").style.display = "none";
@@ -11,14 +11,62 @@ document.getElementById("dom_events").style.display = "none";
 // the user has gotten (integer to represent array index)
 let current_sequence = 0;
 
+function style_sequeunce() {
+	let html = document.getElementById("html");
+	let clock = document.getElementById("clock");
+	let details = document.getElementById("details");
+	switch(current_sequence){
+		case 10:
+			// Alight details text to the right of screen.
+			details.style.textAlign = "right";
+		case 9:
+		case 8:
+			// Generate and display footer.
+			details.innerHTML ="Created by Zachary Morrell <br>CIT 215 JavaScript<br>Professor Jeremy Doolin";
+		case 7:
+		case 6:
+			// Increase Clock Font Size.
+			clock.style.fontSize = "30px";
+			html.style.fontSize = "20px";
+		case 5:
+		case 4:
+			// Center Contents
+			html.style.maxWidth = "600px";
+			html.style.margin = "auto";
+			clock.style.textAlign = "center";
+		case 3:
+		case 2:
+			// Change Background color and font color.
+			html.style.backgroundColor = "black";
+			html.style.color = "white";
+		case 1:
+		break;
+		default:
+			html.style.backgroundColor = "white";
+			html.style.color = "black";
+			html.style.maxWidth = "100%";
+			html.style.margin = "0";
+			html.style.fontSize = "16px";
+			clock.style.fontSize = "16px";
+			clock.style.textAlign = "left";
+			details.innerHTML = "";
+			details.textAlign = "left";
+		break;
+	}
+}
+
 // Add an event listener to the 'document' object
 // that calls a function on a key down event
 
 window.addEventListener("keydown", konamiKeyHandler);
 
 function konamiKeyHandler(event) {
-	let key_pressed = event.key;
-	konami(key_pressed);
+	if(style_sequeunce != 10) {
+		let key_pressed = event.code;
+		console.log(key_pressed);
+		konami(key_pressed);
+		style_sequeunce();
+	}
 }
 
 // Write the function that gets called when a key is down
@@ -62,3 +110,31 @@ function display_answers() {
 // Updates the countdown display in the HTML element
 // Consider using a Date object and getting the numbers
 // from there
+style_sequeunce();
+var target_date = new Date("May 11, 2024 12:00:00").getTime();
+setInterval(run_timer, 1000);
+
+function run_timer() {
+	// Get today's date and time
+	var now = new Date().getTime();
+  
+	// Find the distance between now and the count down date
+	var distance = target_date - now;
+  
+	// Time calculations for days, hours, minutes and seconds
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+	// Display the result in the element with id="demo"
+	document.getElementById("clock").innerHTML = ":.::.: Graduating In :.::.:<br>" + days + " Days " + hours + " Hours "
+	+ minutes + " Minutes " + seconds + " Seconds ";
+  
+	// If the count down is finished, write some text
+	if (distance < 0) {
+	  clearInterval(start_timer());
+	  document.getElementById("demo").innerHTML = "EXPIRED";
+	}
+
+}
